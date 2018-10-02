12db42a1f3ca4839c58d083d7852876dc8902f94
killswitchq <33583519+killswitchq@users.noreply.github.com>
2018-10-02 11:24:15 -0400

    works?

--- VR/Assets/Scripts/FruitSpawn.cs
+++ VR/Assets/Scripts/FruitSpawn.cs
@@ -10,23 +10,25 @@ public class FruitSpawn : MonoBehaviour {
     public int spawnDiff;
 
     void Start () {
-		for(int i = 0; i < spawnPoints.Length; i++) {
-            spawnFruit(fruits, i);
-        }
+
 	}
 
     public void spawnFruit(GameObject[] fruits, int spawnPoint) {
-        int fruitGroup = Random.Range(0, fruits.Length);
+        int fruitGroup = UnityEngine.Random.Range(0, fruits.Length);
         for(int i = 0; i < fruitPerBatch; i++)
         {
             Transform newPoint = spawnPoints[spawnPoint].transform;
-            newPoint.position += new Vector3(Random.Range(-spawnDiff, spawnDiff), 0, Random.Range(-spawnDiff, spawnDiff));
+            newPoint.position += new Vector3(UnityEngine.Random.Range(-spawnDiff, spawnDiff), 0, UnityEngine.Random.Range(-spawnDiff, spawnDiff));
             Instantiate(fruits[fruitGroup], spawnPoints[spawnPoint].transform);
         }
     }
 
     internal void isActiveAndEnabled(bool v)
     {
+        for (int i = 0; i < spawnPoints.Length; i++)
+        {
+            spawnFruit(fruits, i);
+        }
         throw new NotImplementedException();
     }
 }
