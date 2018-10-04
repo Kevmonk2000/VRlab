562ad69317fe61855cbc4de7001425f6bc0b6646
Kevmonk2000 <37087819+Kevmonk2000@users.noreply.github.com>
2018-10-04 10:43:14 -0400

    new

--- VR/Assets/Scripts/ExitButton.cs
+++ VR/Assets/Scripts/ExitButton.cs
@@ -3,37 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Button : MonoBehaviour
+public class ExitButton : MonoBehaviour
 {
-    float timeLeft = 120f;
     public TextMesh countText;
     bool lol = false;
 
     private void Start()
     {
-  
+
     }
     private void Update()
     {
-        
+
         if (lol == true)
         {
-            timeLeft -= Time.deltaTime;
-            countText.text = "Time Left: " +Mathf.Floor(timeLeft).ToString();
+            //timeLeft -= Time.deltaTime;
+            //countText.text = "Time Left: " + Mathf.Floor(timeLeft).ToString();
         }
-        
+
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            lol = true;
 
-            this.GetComponent<FruitSpawn>().isActiveAndEnabled(true);
 
+            OVRPlayerController Acceleration = gameObject.GetComponent<OVRPlayerController>();
+
+            
         }
     }
-    
+
 
 }
